e41103210b19b6819969ee999ea6c905537c8890
Walter1G <2760walter@gmail.com>
2024-01-04 13:44:31 +0300

    add Relationshiop

--- website/models.py
+++ website/models.py
@@ -9,9 +9,12 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.string(150))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #Relationship
     
 
 class Note(db.Model):
     id= db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    notes = db.relationship('Note')
+
